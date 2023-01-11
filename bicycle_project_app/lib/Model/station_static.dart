1646f31f83f3576325a4a17cd7cb68925ae3e9d3
hanbyeol <whdlgksquf5@naver.com>
2023-01-11 17:44:31 +0900

    지도 api에 현재 거치대수 api 합침!!

--- bicycle_project_app/lib/Model/station_static.dart
+++ bicycle_project_app/lib/Model/station_static.dart
@@ -1,3 +1,8 @@
 class StationStatic {
   static int stationNum = 0;
+
+  static String parking2301 = "";
+  static String parking2342 = "";
+  static String parking2348 = "";
+  static String parking2384 = "";
 }
