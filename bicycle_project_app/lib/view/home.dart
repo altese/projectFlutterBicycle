5845ca5404f13b9163839a0424feb262c47e5aea
altese <readme42u@gmail.com>
2023-01-12 14:04:36 +0900

    마이페이지 연결

--- bicycle_project_app/lib/view/home.dart
+++ bicycle_project_app/lib/view/home.dart
@@ -5,7 +5,7 @@ import 'package:bicycle_project_app/view/home_page.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'home2.dart';
-import 'home4.dart';
+import 'my_page.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -26,7 +26,13 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     //바텀 바 아이콘
-    List pages = const [HomePage(), SimpleMap(), Home2(), ChartPage(), Home4()];
+    List pages = const [
+      HomePage(),
+      SimpleMap(),
+      Home2(),
+      ChartPage(),
+      MyPage()
+    ];
     final items = [
       const Icon(Icons.home, size: 30),
       const Icon(Icons.map_rounded, size: 30),
